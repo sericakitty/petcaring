5eec263a53c0db2e9c5f0a3ed8bd068fc4fbfdb7
sericakitty <63978546+sericakitty@users.noreply.github.com>
2025-02-10 18:56:43 +0200

    Enhance login functionality to support username or email input and reset fields on focus loss

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useFocusEffect } from "@react-navigation/native";
 import {
   View,
   Text,
@@ -8,7 +9,15 @@ import {
   Pressable,
 } from "react-native";
 import { auth } from "../../firebaseConfig";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { AuthError, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+  FirestoreError,
+} from "firebase/firestore";
 import { TabNavigationProps } from "../types/navigation";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
@@ -18,23 +27,87 @@ type LoginViewProps = {
 
 /**
  * Login view component
- * This component allows users to login with email and password
+ * This component allows users to login with email or username and password
  *
  * @param navigation - navigation prop for routing
  * @returns Login view component
  */
 const LoginView: React.FC<LoginViewProps> = ({ navigation }) => {
-  const [email, setEmail] = useState<string>("");
+  const [credential, setCredential] = useState<string>();
   const [password, setPassword] = useState<string>("");
   const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [isUsername, setIsUsername] = useState<boolean>(false);
 
-  const handleLogin = async () => {
+  const handleUserNameOrEmailInput = (text: string) => {
+    if (text.includes("@")) {
+      setIsUsername(false);
+    } else {
+      setIsUsername(true);
+    }
+    setCredential(text);
+  };
+
+  const resetInputFields = () => {
+    setCredential("");
+    setPassword("");
+    setShowPassword(false);
+    setIsUsername(false);
+  };
+
+  // Reset input fields when screen loses focus
+  useFocusEffect(
+    React.useCallback(() => {
+      return () => {
+        resetInputFields();
+      };
+    }, []),
+  );
+
+  const handleLogin = async (e: any) => {
+    e.preventDefault();
+    if (!credential || !password) {
+      Alert.alert("Error", "Please fill in all fields!");
+      return;
+    }
+    let emailToUse = credential;
+
+    // Check if username is used instead of email
+    if (isUsername) {
+      try {
+        const db = getFirestore();
+        const usersRef = collection(db, "users");
+        const q = query(usersRef, where("userName", "==", credential));
+        const querySnapshot = await getDocs(q);
+
+        if (querySnapshot.empty) {
+          Alert.alert("Error", "Invalid username!");
+          return;
+        }
+        // Get email from username and set it to emailToUse, which will be used for login
+        emailToUse = querySnapshot.docs.map((doc) => doc.data())[0].email;
+
+        if (!emailToUse) {
+          Alert.alert("Error", "No email found for this username!");
+          return;
+        }
+      } catch (error: any) {
+        Alert.alert("Error", "Failed to retrieve user: " + error.message);
+        return;
+      }
+    }
+    console.log("emailtouse", emailToUse);
     try {
-      await signInWithEmailAndPassword(auth, email, password);
+      await signInWithEmailAndPassword(auth, emailToUse, password);
       Alert.alert("Success", "Login successful!");
+      resetInputFields();
       navigation.navigate("Home");
     } catch (error: any) {
-      Alert.alert("Error", error.message);
+      error = error as AuthError;
+      console.log("error", error);
+      if (error.code === "auth/user-not-found") {
+        Alert.alert("Error", "Invalid credentials!");
+        return;
+      }
     }
   };
 
@@ -42,10 +115,10 @@ const LoginView: React.FC<LoginViewProps> = ({ navigation }) => {
     <View style={styles.container}>
       <Text style={styles.title}>Login</Text>
       <TextInput
-        placeholder="Email"
+        placeholder="Email or Username"
         style={styles.input}
-        onChangeText={setEmail}
-        value={email}
+        onChangeText={handleUserNameOrEmailInput}
+        value={credential}
       />
       <View style={styles.passwordContainer}>
         <TextInput
